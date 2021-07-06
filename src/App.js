e6608f839ff0a162ca1704d93bf79e173b7d6fc5
Administrator <emmanuel.mbaba32@gmail.com>
2021-07-06 09:54:36 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import react from "react";
+import Home from './components/Home';
+import Login from './components/Login';
+import Bark from './components/Bark';
 import './App.css';
+import { Route, NavLink, HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div>
+        <ul className="header">
+          <li><NavLink exact to ="/">Home</NavLink></li>
+          <li><NavLink to="/bark">Bark</NavLink></li>
+          <li><NavLink to="/login">Login</NavLink></li>
+        </ul>
+      </div>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/bark" component={Bark} />
+        <Route exact path="/login" component={Login} />
+      </div>
+    </HashRouter>
+    
   );
 }
 
